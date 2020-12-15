ed57ff2d6877505d50f55797b965ed3d2a3dc260
Sid Menon <siddharth.menon@motional.com>
2020-12-15 12:47:38 -0500

    Make actions work again, and add some auth to the uploadFromCSV function

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # abbreviations-slack-app
 Slack app and Web GUI for expanding abbreviations used within an organization
+
+
+## 1. Start emulator
+
+For debugging:
+
+```sh
+firebase emulators:start --inspect-functions
+```
+
+Without debugging needs:
+
+```sh
+firebase emulators:start --inspect-functions
+```
+
+## 2. Viewing the app with the live Firestore db
+
+```sh
+firebase serve
+```
+
+Now visit `localhost:5000`
+
+
+## 3. Viewing the app with the emulated Firestore db
+
+Run the steps in section `1`. Then run:
+
+```sh
+cd abbreviations-viewer
+ng serve
+```
+
+Now visit `localhost:4200`
+
+
+If you just started the emulator, there won't be any data. To seed this with real data (using the December 2020 snapshot of the app) do this:
+
+1. Remove the call to `verifyToken` in the function `uploadFromCSV`, in the file `functions\src\index.ts`
+1. Restart the emulator (step `1.`)
+1. Run this:
+
+    ```sh
+    curl -X POST http://localhost:5001/sidmenon-playground/us-central1/uploadFromCSV
+    ```
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -16,25 +16,80 @@ interface DefinitionItem {
   contributor: string;
 }
 
+const getToken =
+    async(request: functions.Request): Promise<string|undefined> => {
+  if (!request.headers.authorization) {
+    return undefined;
+  }
+
+  const token: string = request.headers.authorization.replace(/^Bearer\s/, '');
+
+  return token;
+};
+
+/**
+ * Authenticate users based on
+ * https://cloud.google.com/endpoints/docs/openapi/authenticating-users-google-id
+ */
+const verifyToken = async(request: functions.Request): Promise<boolean> => {
+  try {
+    const token: string|undefined = await getToken(request);
+
+    if (!token) {
+      return false;
+    }
+
+    return await admin.auth().verifyIdToken(token).then(async decodedToken => {
+      const uid = decodedToken.uid;
+      const userInfo = await admin.auth().getUser(uid);
+      return (
+          userInfo.email === 'siddharth.menon@motional.com' &&
+          userInfo.emailVerified);
+    });
+  } catch (err) {
+    return false;
+  }
+};
+
+/**
+ * call using:
+ * curl -X POST -H "Authorization: Bearer $(getAccessToken)"
+ \
+              http://localhost:5001/sidmenon-playground/us-central1/uploadFromCSV/snapshot_1607451754712
+ */
 export const uploadFromCSV =
     functions.https.onRequest(async (request, response) => {
-      const csvFileName: string = request.params[0];
-      let collectionName = 'entries';
-      if (request.query.collection) {
-        collectionName = request.query.collection as string;
-      }
-      const pathReference = storage.bucket().file(`acronyms${csvFileName}.csv`);
-      const destFilename = './tmpDownload.csv';
-      const options = {
-        // The path to which the file should be downloaded, e.g. "./file.txt"
-        destination: destFilename
-      };
-      await pathReference.download(options);
-      await csv().fromFile(destFilename).then((jsonValues) => {
-        jsonValues.forEach(async jsonValue => {
-          await db.collection(collectionName).add(jsonValue as DefinitionItem);
+      try {
+        if (!await verifyToken(request)) {
+          response.sendStatus(403);
+          return;
+        }
+        const csvFileName: string = request.params[0];
+        let collectionName = 'entries';
+        if (request.query.collection) {
+          collectionName = request.query.collection as string;
+        }
+        const pathReference =
+            storage.bucket().file(`acronyms${csvFileName}.csv`);
+        if (!pathReference.exists()) {
+          response.sendStatus(404);
+          return;
+        }
+        const destFilename = './tmpDownload.csv';
+        const options = {
+          // The path to which the file should be downloaded, e.g. "./file.txt"
+          destination: destFilename
+        };
+        await pathReference.download(options);
+        await csv().fromFile(destFilename).then((jsonValues) => {
+          jsonValues.forEach(async jsonValue => {
+            await db.collection(collectionName)
+                .add(jsonValue as DefinitionItem);
+          });
         });
-      });
+      } catch (error) {
+        response.sendStatus(500);
+      }
       response.send('Done!');
     });
 
